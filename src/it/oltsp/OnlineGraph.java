bbb25b45cc458964fa40676ea40a1995ea052747
Stefano Rodi <stefano.rodi@gmail.com>
2011-05-18 15:49:36 +0200

    added OnlineGraphPath

--- src/it/oltsp/OnlineGraph.java
+++ src/it/oltsp/OnlineGraph.java
@@ -13,4 +13,5 @@ public interface OnlineGraph<V, E, T> extends WeightedGraph<V, E> {
 	public Graph<V, E> getVisibleGraph(T t);
 	public T getReleaseTime(V v);
 	public void setReleaseTime(V v, T t);
+	public T getNextReleaseTime(T t);
 }
--- src/it/oltsp/alg/Gtr.java
+++ src/it/oltsp/alg/Gtr.java
@@ -4,19 +4,37 @@ import it.oltsp.OnlineGraph;
 import it.oltsp.OnlineGraphPath;
 
 import java.util.Map;
+import java.util.Set;
 
+import org.jgrapht.Graph;
 import org.jgrapht.GraphPath;
 import org.jgrapht.graph.GraphPathImpl;
+import org.jgrapht.traverse.ClosestFirstIterator;
 
 public class Gtr<V, E, T> {
 
 	OnlineGraph<V, E, T> onlineGraph;
+	V startVertex;
+	Set<V> vertexes;
 
-	public Gtr(OnlineGraph<V, E, T> graph) {
+	public Gtr(OnlineGraph<V, E, T> graph, V startVertex) {
 		this.onlineGraph = graph;
+		this.startVertex = startVertex;
+		this.vertexes = graph.vertexSet();
 	}
 	
 	public OnlineGraphPath<V, E, T> getVisitingPath() {
+
+		V actualVertex = startVertex;
+		T actualTime = null;
+		Graph<V, E> visibleGraph = onlineGraph.getVisibleGraph(actualTime);
+		ClosestFirstIterator<V, E> itr = new ClosestFirstIterator<V, E>(visibleGraph, actualVertex);
+		if(itr.hasNext())
+			itr.next();
+		
+	    
+		
+		
 		return null; //TODO
 	}
 }
